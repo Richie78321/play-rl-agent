d74b748ed3b3f448bb5311f283604c0773d26b7e
Richie Goulazian <rgoulazian@gmail.com>
2023-04-10 21:50:33 +0000

    Update the data types of the states and actions

--- postgres.sql
+++ postgres.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS playdata (
     id SERIAL PRIMARY KEY,
-    initial_state TEXT NOT NULL,
-    action TEXT NOT NULL,
-    resultant_state TEXT NOT NULL,
+    initial_state INTEGER NOT NULL,
+    action INTEGER NOT NULL,
+    resultant_state INTEGER NOT NULL,
     reward REAL NOT NULL
 )
